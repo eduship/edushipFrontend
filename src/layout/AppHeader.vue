cb516d3eaca0a285fdab678dd08047d8dc5c8b75
elisabethschiele <elisabethschiele@gmail.com>
2018-09-22 21:23:17 +0200

    header nochmal weiter... oder so

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -50,14 +50,17 @@
         </div>
     </div>
 </nav>
-  </div>
+</div>
+</div>
 </template>
 <script>
+import BaseNav from "@/components/BaseNav";
 import BaseDropdown from "@/components/BaseDropdown";
 import CloseButton from "@/components/CloseButton";
 
 export default {
   components: {
+    BaseNav,
     CloseButton,
     BaseDropdown
   }
