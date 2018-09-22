3c98f5058e75af7a1102626c9eb26c3655020656
Rubozs <Ruben2412000@gmail.com>
2018-09-22 15:30:19 +0200

    router erstellt

--- src/router.js
+++ src/router.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import Router from "vue-router";
 import AppHeader from "./layout/AppHeader";
 import AppFooter from "./layout/AppFooter";
-import Map from "./views/map.vue";
+import Start from "./views/map.vue";
+import Impressum from "./views/impressum.vue";
+import SingleEvent from "./views/singleEvent.vue";
 
 Vue.use(Router);
 
@@ -12,7 +14,26 @@ export default new Router({
     path: "/",
     name: "start",
     components: {
+      header: AppHeader,
       default: Start,
+      footer: AppFooter
+    }
+  },
+  {
+    path: "/impressum",
+    name: "impressum",
+    components: {
+      header: AppHeader,
+      default: Impressum,
+      footer: AppFooter
+    }
+  },
+  {
+    path: "/event",
+    name: "event",
+    components: {
+      header: AppHeader,
+      default: SingleEvent,
       footer: AppFooter
     }
   }
