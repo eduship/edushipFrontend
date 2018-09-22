4f2cd3c66cc612bb5c126fe8b5fd526293c0f7c9
Jana-Z <github3@jana-zeller.de>
2018-09-22 15:18:37 +0200

    Datenstruktur angelegt

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Router from "vue-router";
+import AppHeader from "./layout/AppHeader";
+import AppFooter from "./layout/AppFooter";
+import Start from "./views/start.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  linkExactActiveClass: "active",
+  routes: [{
+    path: "/",
+    name: "start",
+    components: {
+      default: Start,
+      footer: AppFooter
+    }
+  }
+  ],
+  scrollBehavior: to => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
+});
