0fe1772eb6727e5126a0ca3f9c294de2e0c01e80
Jana-Z <github3@jana-zeller.de>
2018-09-24 12:14:33 +0200

    About page noch mit Blindtext

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,4 +1,21 @@
 <template>
-<div>
-</div>
+  <div class="row">
+    <div class="col-sm">
+      <collaboratorComp namedC="Sophia von den Bergen" v-bind:randomFact = "['Lieblingsfarbe: #0000', 'Ich hasse alles']"> </collaboratorComp>
+    </div>
+    <div class="col-sm">
+      <collaboratorComp v-bind:buttonId = "2" namedC="Fynn" v-bind:randomFact="['Ist der schrullige Programmierer des Teams', 'Mag den Pina Colada Song']"> </collaboratorComp>
+    </div>
+  </div>
 </template>
+
+<script>
+import collaboratorComp from "@/components/collaboratorComp";
+
+export default {
+  components: {
+    collaboratorComp
+  },
+};
+
+</script>
