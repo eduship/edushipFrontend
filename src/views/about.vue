e0ab1fba276ea1d2abd49fda5d10ea8269859a71
Jana-Z <github3@jana-zeller.de>
2018-09-24 19:22:22 +0200

    about page ist jetzt schöner, mit "Header"

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,4 +1,30 @@
 <template>
+  <div class="position-relative">
+           <!-- shape Hero -->
+           <section class="section-profile-cover section-shaped my-0">
+            <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+               </div>
+               <div class="container shape-container d-flex">
+                   <div class="col px-0">
+                       <div class="row">
+                           <div class="col-lg-6">
+                               <h1 class="display-3  text-white">Hallo
+                                   <span>Wir sind eduship</span>
+                               </h1>
+                             </div>
+                      </div>
+                  </div>
+                </div>
+            </section>
   <div class="row">
     <div class="col-sm">
       <collaboratorComp namedC="Sophia von den Bergen" v-bind:randomFact = "['Lieblingsfarbe: #0000', 'Ich hasse alles']"> </collaboratorComp>
@@ -7,6 +33,7 @@
       <collaboratorComp v-bind:buttonId = "2" namedC="Fynn" v-bind:randomFact="['Ist der schrullige Programmierer des Teams', 'Mag den Pina Colada Song']"> </collaboratorComp>
     </div>
   </div>
+</div>
 </template>
 
 <script>
