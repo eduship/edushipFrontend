f0816c188d41f8798aaa49a910323d4c7b0b6635
Jana-Z <github3@jana-zeller.de>
2018-10-17 22:20:51 +0200

    favicon geändert und about.vue hinzugefügt

---
+++ src/views/about.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="position-relative">
+           <!-- shape Hero -->
+           <section class="section-profile-cover section-shaped my-0">
+            <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+                   <span></span>
+               </div>
+               <div class="container shape-container d-flex">
+                   <div class="col px-0">
+                       <div class="row">
+                           <div class="col-lg-6">
+                               <h1 class="display-3  text-white">Hallo
+                                   <span>Wir sind eduship</span>
+                               </h1>
+                             </div>
+                      </div>
+                  </div>
+                </div>
+            </section>
+  <div class="row">
+    <div class="col-sm">
+      <collaboratorComp namedC="Elisabeth" v-bind:randomFact = "['Lieblingsspitzname: Elsbet', 'Lieblingsfarbe: #033A0B', 'Lieblingsbuch: Harry Potter', 'hört gerne: Känguru Chroniken']"> </collaboratorComp>
+    </div>
+    <div class="col-sm">
+      <collaboratorComp v-bind:buttonId = "2" namedC="Moritz" v-bind:randomFact="['Lieblingsfarbe: #F16027', 'Lieblingsbiermarke: Oettinger', 'Lieblingsspruch: Die Standart eines Flamingos ist einbeinig', 'war Mensch der Woche in der Bild der Frau']"> </collaboratorComp>
+    </div>
+  </br>
+    <div class="col-sm">
+      <collaboratorComp namedC="Jana" v-bind:buttonId = "3" v-bind:randomFact = "['Lieblingsfarbe: #912453', 'Lieblingsfilm: Forrest Gump', 'Lieblingsbuch: Harry Potter', 'Lieblingsserie: Game of Thrones']"> </collaboratorComp>
+    </div>
+    <!-- <div class="col-sm">
+      <collaboratorComp v-bind:buttonId = "4" namedC="Moritz" v-bind:randomFact="['Lieblingsfarbe: #F16027', 'Lieblingsbiermarke: Oettinger', 'Lieblingsspruch: Die Standart eines Flamingos ist einbeinig', 'war Mensch der Woche in der Bild der Frau']"> </collaboratorComp>
+    </div> -->
+  </div>
+</div>
+</template>
+
+<script>
+import collaboratorComp from "@/components/collaboratorComp";
+
+export default {
+  components: {
+    collaboratorComp
+  },
+};
+
+</script>
