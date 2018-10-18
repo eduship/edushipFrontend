41b6835a18e82ad69bdfddd96cf20f4f5f02f55d
elisabethschiele <elisabethschiele@gmail.com>
2018-10-18 19:02:02 +0200

    linkung und farben und son zeug

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,7 +3,7 @@
       <div class="position-relative">
           <!-- shape Hero -->
           <section class="section-shaped my-0">
-              <div class="shape shape-style-1 shape-default shape-skew">
+              <div class="shape shape-primary shape-skew">
                   <span></span>
                   <span></span>
                   <span></span>
