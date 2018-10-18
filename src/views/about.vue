a900e3b43f96fde7383d24b2697424a57a3c194e
Jana-Z <github3@jana-zeller.de>
2018-10-18 18:13:38 +0200

    Punkte oben weg

--- src/views/about.vue
+++ src/views/about.vue
@@ -2,7 +2,7 @@
   <div class="position-relative">
            <!-- shape Hero -->
            <section class="section-profile-cover section-shaped my-0">
-            <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
+            <div class="shape shape-primary shape-skew">
                    <span></span>
                    <span></span>
                    <span></span>
