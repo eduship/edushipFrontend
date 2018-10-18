7e8c47da51490344f63cf8e05bc2a083984b999a
elisabethschiele <elisabethschiele@gmail.com>
2018-10-18 17:08:30 +0200

    case sensitivity

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import Landing from "./views/Landing.vue";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
 import Profile from "./views/Profile.vue";
-import About from "./views/about.vue";
+import About from "./views/About.vue";
 
 Vue.use(Router);
 
