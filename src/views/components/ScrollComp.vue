347760a2e0f95784993c16d9b682f127c18c46ad
Moritz Beck <thecrafthalltekkit@web.de>
2018-10-21 01:31:25 +0200

    new eventComp

--- src/views/components/ScrollComp.vue
+++ src/views/components/ScrollComp.vue
@@ -3,26 +3,36 @@
   <div id="app" align="center">
     <h1 class="centered">Events</h1>
     <div class="box scrollcomp">
-      <EventComp eventName="Code und Design Camp Leipzig" eventDate="18.-21.10.18" eventLevel="entry"></EventComp>
-      <EventComp eventName="Code und Design Camp @Telekom Düsseldorf" eventDate="22.-27.10.18" eventLevel="entry"></EventComp>
-      <EventComp eventName="Hacker School Hamburg" eventDate="27.-28.10.18" eventLevel="entry"></EventComp>
-      <EventComp eventName="Code und Design Meetup Frankfurt" eventDate="31.10.18" eventLevel="entry"></EventComp>
-      <EventComp eventName="Code und Desin Camp Berlin" eventDate="01.-04.11.18" eventLevel="entry"></EventComp>
-      <EventComp eventName="Jugend Hackt Heidelberg" eventDate="09.-11.11.18" eventLevel="advanced"></EventComp>
-      <EventComp eventName="Code und Design Meetup München" eventDate="21.11.18" eventLevel="entry"></EventComp>
-      <EventComp eventName="Hacker School Hamburg" eventDate="23.-24.11.18" eventLevel="entry"></EventComp>
-      <EventComp eventName="Jugend Hackt Ulm" eventDate="23.-25.11.18" eventLevel="advanced"></EventComp>
-      <EventComp eventName="Hacker School Hamburg" eventDate="30.11.-1.12.18" eventLevel="entry"></EventComp>
+       <li v-for="item in events">
+        <EventComp :eventName="item.name" :eventLocation="item.placeAdress" :eventDate="item.date" :eventLevel="item.level" :eventTag="item.tags"></EventComp>
+      </li>
     </div>
   </div>
 </template>
 
 <script>
 import EventComp from "@/components/EventComp";
+import axios from 'axios';
 export default {
   name: "ScrollComp",
   components: {
     EventComp
+  },
+  data() {
+    return {
+      events: [],
+      errors: []
+    }},
+  created() {
+    axios.get(`http://localhost:5000/event/all`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.events = response.data;
+      //console.log(this.events);
+    })
+    .catch(e => {
+      this.errors.push(e);
+    })
   }
 }
 </script>
